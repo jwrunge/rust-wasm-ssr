aa348650b91c487a23f926cc3ce7298d9f0f7274
jrunge <jrunge@precisionplanting.com>
2023-11-02 14:03:39 -0500

    Minor update to config behaviors test failure msg

--- src/config.rs
+++ src/config.rs
@@ -218,7 +218,7 @@ mod tests {
         let config = get_test_config(TestConfig::Uninitialized);
         assert!(match *config.default_behavior.borrow() {
             Some(ExtensionBehavior::Deny) => true,
-            _ => panic!("Default behavior should be 'Deny' or 'ProcessCode'"),
+            _ => panic!("Default behavior should be 'Deny' if not otherwise specified"),
         });
     }
 }
